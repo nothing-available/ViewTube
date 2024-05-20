81ed5ae81799ac178448ad1a8c9c232fc2029af8
nothing-available <smtkur31@gmail.com>
2024-05-21 03:40:51 +0530

    add Subscription model

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -4,7 +4,6 @@ import { User } from "../models/user.model.js"
 import { uploadOnCloudinary } from '../utils/cloudinary.js'
 import { ApiResponse } from '../utils/apiResponse.js'
 import jwt from "jsonwebtoken"
-import { application } from "express"
 
 
 
